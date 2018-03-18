5b670865e62168412cb8149c91368e2d1570dffe
z0u5 <35923701+z0u5@users.noreply.github.com>
2018-03-19 00:08:13 +0400

    Update QWERT.py

--- QWERT.py
+++ QWERT.py
@@ -1,1 +1,20 @@
-print "What"
+import socket
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 8080
+f = open("host.txt","w")
+f.write(str(HOST))
+f.close()
+s = socket.socket()
+s.bind((HOST, PORT))
+s.listen(10)
+conn, addr = s.accept()
+print 'Connected by', addr
+conn.send("hi")
+
+while True:
+    print "v"
+    data = conn.recv(1024)
+    print "yes"
+    conn.send(data)
+conn.close()
